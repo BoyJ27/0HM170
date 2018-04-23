dff3ca9f6c021ab03ed7ad637c6c561360e105cd
Janissen <b.n.j.janissen@student.tue.nl>
2018-04-23 22:35:15 +0200

    Enable butten after timeout
    Current timeout set to 3s.

--- js/view/playView.js
+++ js/view/playView.js
@@ -5,7 +5,7 @@ var PlayView = function (model, container){
 	var h1 = $("<p>U kunt nu MasterMind spelen. Na 8 minuten wordt de 'Volgende'-knop klikbaar. Na de klik krijgt u een korte tussentijdse enquete te zien, waarna u weer verder kunt gaan met spelen.</p>");
 	var h2 = $("<p>U kunt uw deelname afronden door op 'Beëindig Experiment' te klikken.</p>")
 	var volgendeButtonCont		= $("<center></center>");
-  var volgendeButton    = $( "<a class='btn button btn-default pull-right' role='button'>Volgende &raquo;</a>" );
+  var volgendeButton    = $( "<button class='btn button btn-default pull-right' style='color:black;' id='timedbutton' role='button' disabled>Volgende &raquo;</button>" );
 	var stoppenCont		= $("<center></center>");
 	var stoppen = $( "<a class='btn button btn-default pull-right' id='stoppen' role='button'>Experiment Beëindigen</a>" )
   var clearfix          = $( '<div class="clearfix">' );
@@ -23,6 +23,7 @@ var PlayView = function (model, container){
 		if( args == "introPlayDone"){
       container.show();
 			if (model.gamecount == 1){
+			setTimeout(function(){ $('#timedbutton').prop("disabled", false);; }, 3000);
 			textCont.append(h1);
 			buttonCont.append(volgendeButtonCont);
 
