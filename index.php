f61caaa8d65f988b972a4c8d49009b9b5cd352c9
JochemBek <Jochem_bek@hotmail.com>
2018-05-08 12:08:18 +0200

    Revert "Revert "DB connection""
    This reverts commit e7e0ba4d2c8deafdedcea874dbdad2552c9277c9.

--- index.php
+++ index.php
@@ -28,8 +28,7 @@
         <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-        <script src="TemplateData/UnityProgress.js"></script>  
-	    <script src="Build/UnityLoader.js"></script>
+        
         
 
     </head>
